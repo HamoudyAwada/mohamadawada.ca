a052fbb6b835c9a60b710039c958aa24523a4a61
HamoudyAwada <moeawada04@gmail.com>
2026-04-13 18:06:20 -0600

    Set up google analytics

--- src/app/components/RootLayout.tsx
+++ src/app/components/RootLayout.tsx
@@ -1,6 +1,20 @@
-import { Outlet, ScrollRestoration } from 'react-router';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router';
+import { useEffect } from 'react';
+
+declare function gtag(...args: unknown[]): void;
 
 export default function RootLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof gtag !== 'undefined') {
+      gtag('event', 'page_view', {
+        page_path: location.pathname,
+        page_location: window.location.href,
+      });
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <ScrollRestoration />
